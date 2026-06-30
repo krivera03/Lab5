c87e47a865e947f62776831ca91f624cb7ca9096
Karen Rivera <k.riveraangulo@outlook.com>
2026-06-29 23:42:04 -0600

    Se crea el ingreso del nodo al inicio de la lista

--- funciones.c
+++ funciones.c
@@ -1,6 +1,9 @@
-#include <stdlib.h>
-#include "lista.h"
 
+
+
+/*------Listas Enlazadas--------*/
+
+/*Crea un nuevo nodo con el dato indicado por el usuario*/
 Nodo *nodo_crear(int dato) {
     Nodo *nuevo = malloc(sizeof(Nodo));
 
@@ -8,8 +11,19 @@ Nodo *nodo_crear(int dato) {
         return NULL;
     }
 
-    nuevo->dato = dato;
+    nuevo->dato = dato;     /*Asinga el valor al nodo*/
     nuevo->next = NULL;
 
     return nuevo;
+}
+
+
+/*Ingresa un nodo al inicio de la lista*/
+void lista_insertar_inicio(Nodo **head, int dato) {
+    Nodo *nuevo = nodo_crear(dato);
+
+    if (nuevo == NULL) return;
+
+    nuevo->next = *head;     /*El nuevo nodo apunta al anterior primer nodo*/
+    *head = nuevo;
 }
--- funciones.h
+++ funciones.h
@@ -1,11 +1,20 @@
 #ifndef LISTA_H
 #define LISTA_H
 
+/*Estructura que representa un nodo de la lista enlanzada*/
 typedef struct Nodo {
     int dato;
     struct Nodo *next;
 } Nodo;
 
+/*Genera un nodo con un valor específico*/
 Nodo *nodo_crear(int dato);
+
+/*Ingresa un nodo al inicio de la lista*/
+void lista_insertar_inicio(Nodo **head, int dato);
+
+
+
+
 
 #endif
