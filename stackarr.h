16d6dbb9930418bd5564d5863ba1facade6b2c81
IsmaelAlvaradoPerez <147348313+Ismael-Alvarado@users.noreply.github.com>
2026-06-30 17:52:06 -0600

    Lo mismo con stack, se le pone stackarr para aclarar que es con arreglo dinamico

---
+++ stackarr.h
@@ -0,0 +1,15 @@
+#ifndef STACKARR_H
+#define STACKARR_H
+#include "dinarr.h"
+typedef struct {
+    DinaArray arr;
+} StackArr;
+
+void sa_init (StackArr *s, size_t cap_inicial);
+void sa_push (StackArr *s, int valor);
+int  sa_pop  (StackArr *s);            /* retorna el valor extraído */
+int  sa_peek (StackArr *s);            /* retorna el tope sin extraer */
+int  sa_empty(StackArr *s);            /* retorna 1 si está vacío */
+void sa_free (StackArr *s);
+
+#endif
